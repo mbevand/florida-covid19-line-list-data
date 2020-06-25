6ac8852821dc468163e47eda9ab227816eea7a65
Marc Bevand <m.bevand@gmail.com>
2020-06-24 21:57:54 -0700

    refactoring

--- process.py
+++ process.py
@@ -25,6 +25,8 @@ class Buckets():
     cases_per_bracket = {}
     # ages[23] is the list of case ages for week 23
     ages = {}
+    age_unknown = 0
+    total_cases = 0
 
 def week2monday(week):
     # convert ISO week number to the date of the Monday in that week
@@ -41,36 +43,46 @@ def bucket(b, age, date):
             b.cases_per_bracket[week][x] += 1
     b.ages[week].append(age)
 
-b = Buckets()
-unkn = 0
-cases = 0
-first = True
-for l in csv.reader(open('Florida_COVID19_Case_Line_Data.csv')):
-    (County,Age,Age_group,Gender,Jurisdiction,Travel_related,Origin,EDvisit,Hospitalized,Died,Case_,Contact,Case1,EventDate,ChartDate,ObjectId) = l
-    if first:
-        assert ','.join((County,Age,Age_group,Gender,Jurisdiction,Travel_related,Origin,EDvisit,Hospitalized,Died,Case_,Contact,Case1,EventDate,ChartDate,ObjectId)) == '\ufeff' + 'County,Age,Age_group,Gender,Jurisdiction,Travel_related,Origin,EDvisit,Hospitalized,Died,Case_,Contact,Case1,EventDate,ChartDate,ObjectId'
-        first = False
-        continue
-    if Age == 'NA':
-        unkn += 1
-        continue
-    # ChartDate is the date the case was counted according the header of table
-    # "Coronavirus: line list of cases" in
-    # http://ww11.doh.state.fl.us/comm/_partners/action/report_archive/state/state_reports_latest.pdf
-    date = datetime.datetime.strptime(ChartDate.split(' ')[0], '%Y/%m/%d').date()
-    age = int(Age)
-    bucket(b, age, date)
-    cases += 1
-print('''Number of COVID-19 cases per week in Florida, by age bracket.
-Source: https://open-fdoh.hub.arcgis.com/datasets/florida-covid19-case-line-data
-''')
-sys.stdout.write('{:11}'.format('week_of,'))
-for x in age_buckets:
-    sys.stdout.write(' {:02d}-{:02d},'.format(x[0], x[1]))
-sys.stdout.write(' median_age\n')
-for week in sorted(b.cases_per_bracket):
-    sys.stdout.write('{:10},'.format(str(week2monday(week))))
+def parse():
+    b = Buckets()
+    first = True
+    for l in csv.reader(open('Florida_COVID19_Case_Line_Data.csv')):
+        (County,Age,Age_group,Gender,Jurisdiction,Travel_related,Origin,EDvisit,Hospitalized,Died,Case_,Contact,Case1,EventDate,ChartDate,ObjectId) = l
+        if first:
+            assert ','.join((County,Age,Age_group,Gender,Jurisdiction,Travel_related,Origin,EDvisit,Hospitalized,Died,Case_,Contact,Case1,EventDate,ChartDate,ObjectId)) == \
+                 '\ufeff' + 'County,Age,Age_group,Gender,Jurisdiction,Travel_related,Origin,EDvisit,Hospitalized,Died,Case_,Contact,Case1,EventDate,ChartDate,ObjectId'
+            first = False
+            continue
+        b.total_cases += 1
+        if Age == 'NA':
+            b.age_unknown += 1
+            continue
+        # ChartDate is the date the case was counted according the header of table
+        # "Coronavirus: line list of cases" in
+        # http://ww11.doh.state.fl.us/comm/_partners/action/report_archive/state/state_reports_latest.pdf
+        date = datetime.datetime.strptime(ChartDate.split(' ')[0], '%Y/%m/%d').date()
+        age = int(Age)
+        bucket(b, age, date)
+    return b
+
+def show_stats(b):
+    print('''Number of COVID-19 cases per week in Florida, by age bracket.
+    Source: https://open-fdoh.hub.arcgis.com/datasets/florida-covid19-case-line-data
+    ''')
+    sys.stdout.write('{:11}'.format('week_of,'))
     for x in age_buckets:
-        sys.stdout.write(' {:5d},'.format(b.cases_per_bracket[week][x]))
-    sys.stdout.write('  {:.1f}\n'.format(statistics.median(b.ages[week])))
-print("(Last week's data is incomplete. Age unknown for {} out of {} cases)".format(unkn, cases))
+        sys.stdout.write(' {:02d}-{:02d},'.format(x[0], x[1]))
+    sys.stdout.write(' median_age\n')
+    for week in sorted(b.cases_per_bracket):
+        sys.stdout.write('{:10},'.format(str(week2monday(week))))
+        for x in age_buckets:
+            sys.stdout.write(' {:5d},'.format(b.cases_per_bracket[week][x]))
+        sys.stdout.write('  {:.1f}\n'.format(statistics.median(b.ages[week])))
+    print("(Last week's data is incomplete. Age unknown for {} out of {} cases)".format(b.age_unknown, b.total_cases))
+
+def main():
+    b = parse()
+    show_stats(b)
+
+if __name__ == '__main__':
+    main()
