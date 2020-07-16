a082622f2197e9921a5517edd935c0eb0cae69b2
Marc Bevand <m.bevand@gmail.com>
2020-07-16 12:57:39 -0700

    Update README, heatmap
    Add line list data for 7/15 and 7/16
    
    heatmap.py now defaults to 7-day time periods
    
    Only put one heatmap in the README

--- heatmap.py
+++ heatmap.py
@@ -16,7 +16,7 @@ from PIL import Image
 
 # This CSV is found at https://open-fdoh.hub.arcgis.com/datasets/florida-covid19-case-line-data (click Download → Spreadsheet)
 csv_url = "https://opendata.arcgis.com/datasets/37abda537d17458bae6677b8ab75fcb9_0.csv"
-buckets_days = 4
+buckets_days = 7
 buckets_ages = [(0, 4), (5, 9), (10, 14), (15, 19), (20, 24), (25, 29), (30, 34), (35, 39), (40, 44), (45, 49), (50, 54), (55, 59), (60, 64), (65, 69), (70, 74), (75, 79), (80, 84), (85, 89), (90, math.inf), ]
 #buckets_ages = [(0, 9), (10, 19), (20, 29), (30, 39), (40, 49), (50, 59), (60, 69), (70, 79), (80, 89), (90, math.inf), ]
 #buckets_ages = [(i, i) for i in range(100)] + [(100,math.inf)]
