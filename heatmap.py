294055d59cb6c82a2c51e993f36bb32d408601d6
Marc Bevand <m.bevand@gmail.com>
2020-07-20 00:44:34 -0700

    heatmap: many updates
    - create a heatmap of cases per capita (heatmap_per_capita.png)
    
    - label the colorbar
    
    - pick the reference point more intelligently to align time periods

--- heatmap.py
+++ heatmap.py
@@ -17,10 +17,38 @@ from PIL import Image
 # This CSV is found at https://open-fdoh.hub.arcgis.com/datasets/florida-covid19-case-line-data (click Download → Spreadsheet)
 csv_url = "https://opendata.arcgis.com/datasets/37abda537d17458bae6677b8ab75fcb9_0.csv"
 buckets_days = 7
-buckets_ages = [(0, 4), (5, 9), (10, 14), (15, 19), (20, 24), (25, 29), (30, 34), (35, 39), (40, 44), (45, 49), (50, 54), (55, 59), (60, 64), (65, 69), (70, 74), (75, 79), (80, 84), (85, 89), (90, math.inf), ]
+buckets_ages = [(0, 4), (5, 9), (10, 14), (15, 19), (20, 24), (25, 29), (30, 34), (35, 39), (40, 44), (45, 49), (50, 54), (55, 59), (60, 64), (65, 69), (70, 74), (75, 79), (80, 84), (85, math.inf), ]
 #buckets_ages = [(0, 9), (10, 19), (20, 29), (30, 39), (40, 49), (50, 59), (60, 69), (70, 79), (80, 89), (90, math.inf), ]
 #buckets_ages = [(i, i) for i in range(100)] + [(100,math.inf)]
 datadir = 'data_fdoh'
+
+def per_1000(bucket, n):
+    # Given an age bracket and a number of residents in this age bracket,
+    # return the number per 1000 residents. We use the following age
+    # pyramid for Florida, from https://data.census.gov (2018, table S0101)
+    pyramid_florida = {
+            (0,4): 1_135_392,
+            (5,9): 1_127_602,
+            (10,14): 1_244_592,
+            (15,19): 1_234_024,
+            (20,24): 1_255_590,
+            (25,29): 1_419_979,
+            (30,34): 1_338_536,
+            (35,39): 1_324_913,
+            (40,44): 1_261_791,
+            (45,49): 1_351_627,
+            (50,54): 1_390_341,
+            (55,59): 1_449_290,
+            (60,64): 1_406_864,
+            (65,69): 1_291_213,
+            (70,74): 1_113_021,
+            (75,79): 836_522,
+            (80,84): 557_028,
+            (85,math.inf): 561_000,
+            }
+    if bucket not in pyramid_florida:
+        return 0
+    return 1e3 * n / pyramid_florida[bucket]
 
 def bracket2str(bracket):
     if bracket[1] == math.inf:
@@ -50,10 +78,13 @@ def print_stats(cases_per_bracket, ages, df):
         f"{cases_total} cases.)"
     )
 
-def gen_heatmap(cases_per_bracket, filename, comment):
+def gen_heatmap(cases_per_bracket, filename, comment='', sqrt=False, title='', cm='inferno', clabel=''):
     def conv(val):
-        # Square root makes the heat map brighter (by dampening the highest values).
-        return np.sqrt(val)
+        if sqrt:
+            # Square root makes the heat map brighter (by dampening the highest values).
+            return np.sqrt(val)
+        else:
+            return val
     rcParams["figure.titlesize"] = "x-large"
     (fig, ax) = plt.subplots(dpi=300)
     periods = sorted(cases_per_bracket.keys())
@@ -91,12 +122,18 @@ def gen_heatmap(cases_per_bracket, filename, comment):
         transform=ax.transAxes,
         verticalalignment="top",
     )
-    fig.suptitle("Heatmap Of COVID-19 Cases In Florida\nBy Age Over Time")
-    img = ax.imshow(a, cmap="inferno", origin="lower", interpolation="nearest", aspect="auto")
-    ticks = [i * 10**e for e in range(1, 5) for i in (1, 2, 5)]
-    del ticks[0]
-    cbar = fig.colorbar(img, ticks=[conv(x) for x in ticks])
-    cbar.ax.set_yticklabels(ticks)
+    fig.suptitle(f"Heatmap Of COVID-19 Cases In Florida\nBy Age Over Time{title}")
+    img = ax.imshow(a, cmap=cm, origin="lower", interpolation="nearest", aspect="auto")
+    if sqrt:
+        if filename == 'heatmap_per_capita':
+            ticks = list(range(100)) + [0.5]
+        else:
+            ticks = [0, 20] + [i * 10**e for e in range(2, 5) for i in (1, 2, 5)]
+        cbar = fig.colorbar(img, ticks=[conv(x) for x in ticks])
+        cbar.ax.set_yticklabels(ticks)
+    else:
+        cbar = fig.colorbar(img)
+    cbar.set_label(clabel)
     plt.savefig(f"{filename}.png", bbox_inches="tight")
 
 def gen_gif(df):
@@ -155,10 +192,10 @@ def main():
     df["date_parsed"] = pd.to_datetime(
             df["ChartDate"].apply(lambda x: x.split(' ')[0]), format="%Y/%m/%d"
     )
-    # Pick an arbitrary point in time to align the time periods. Today's date is usually
-    # the best choice assuming the CSV file contains current data (ie. data up to yesterday)
-    # because this ensures the last period ends on yesterday
-    reference = datetime.datetime.now().date()
+    # Pick a reference point in time to align the time periods. The date one day past the
+    # last date in the dataset is the best choice because it aligns the last period so it
+    # ends on, and includes, the last date in the dataset.
+    reference = sorted(set(df['date_parsed']))[-1].date() + datetime.timedelta(days=1)
     df["Delta"] = df["date_parsed"].apply(lambda date: (date.date() - reference).days)
     df["Period"] = df["Delta"].apply(
         lambda delta: reference + datetime.timedelta(days=delta - delta % buckets_days)
@@ -181,18 +218,28 @@ def main():
             in_period_and_age = in_period & in_age_bucket
             cases_per_bracket[period][bucket] = in_period_and_age.sum()
             ages[period].extend(list(non_null[in_period_and_age]["Age"]))
+    # calculate share_positive
     share_positive = {}
     for (period, cases_data) in cases_per_bracket.items():
         total_cases = sum(cases_data.values())
         share_positive[period] = {}
         for (bucket, cases) in cases_data.items():
-            share_positive[period][bucket] = cases / total_cases
+            share_positive[period][bucket] = 100 * cases / total_cases
+    # calculate cases_per_capita
+    cases_per_capita = {}
+    for (period, cases_data) in cases_per_bracket.items():
+        cases_per_capita[period] = {}
+        for (bucket, cases) in cases_data.items():
+            cases_per_capita[period][bucket] = per_1000(bucket, cases)
+    # print stats and generate charts
     print_stats(cases_per_bracket, ages, df)
     gen_gif(df)
-    gen_heatmap(cases_per_bracket, "heatmap",
-            "number of cases reported")
-    gen_heatmap(share_positive, "heatmap_age_share",
-            "share of cases in the age bracket among\nall cases in the time period")
+    gen_heatmap(cases_per_bracket, 'heatmap', sqrt=True, clabel='Number of cases',
+            comment='number of cases reported')
+    gen_heatmap(share_positive, 'heatmap_age_share', cm='viridis', clabel='Percentage of cases',
+            title=' (Percentage)', comment='percentage of cases in the age bracket\namong all cases in the time period')
+    gen_heatmap(cases_per_capita, 'heatmap_per_capita', sqrt=True, cm='jet', clabel='Number of cases per 1000 residents',
+            title=' (Per Capita)', comment='number of cases reported per capita')
 
 if __name__ == "__main__":
     main()
